eb352d0f979e800c0f11f182b9c5037a83429618
rohanthakran <rohankumarthakran@gmail.com>
2021-01-27 15:20:57 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react"
+import React , {useState} from "react"
 import './App.css';
 import logo from "./images/logo.png";
 import Post from "./Post.js"
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      username :"clever1",
+      caption :"WOW",
+      imageUrl:"https://www.freecodecamp.org/news/content/images/size/w2000/2021/01/7-react-projects-for-2021-2.png"
+    },
+    {
+      username :"clever",
+      caption :"WOW",
+      imageUrl:"https://www.freecodecamp.org/news/content/images/size/w2000/2021/01/7-react-projects-for-2021-2.png"
+    }
+  ])
   return (
     <React.Fragment>
       {/*Header*/}
@@ -15,6 +28,12 @@ function App() {
           src={logo}/>
 
         </div>
+
+        {
+          posts.map(post => (
+             <Post username={post.username} caption ={post.caption} imageUrl={post.imageUrl} />
+          ))
+        }
         <Post username ="clever" caption ="WOW" imageUrl="https://www.freecodecamp.org/news/content/images/size/w2000/2021/01/7-react-projects-for-2021-2.png"/>
         <Post username ="Rohan" caption ="WOW" imageUrl="https://www.freecodecamp.org/news/content/images/size/w2000/2021/01/7-react-projects-for-2021-2.png"/>
 
